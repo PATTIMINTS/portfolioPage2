6842e583c786e7f71f998f5ca3f0107265115752
Pat Seade <pattimint6@gmail.com>
2018-12-09 16:47:38 -0600

    jQuery

--- port.js
+++ port.js
@@ -1,7 +1,43 @@
 $(function() {
-    $("#imageOne").hover(function() {
-        $(this).find('#one').show(200);
-        (".centered").hide();
+    $("#image-One").hover(function() {
+        $(this).find('#one').toggle(200);
+        
+    
+        
+    });
+});
+$(function() {
+    $("#image-two").hover(function() {
+        $(this).find('#last').toggle(200);
+    
+        
+    });
+});
+$(function() {
+    $("#image-Three").hover(function() {
+        $(this).find('#three').toggle(200);
+    
+        
+    });
+});
+$(function() {
+    $("#image-Four").hover(function() {
+        $(this).find('#four').toggle(200);
+    
+        
+    });
+});
+$(function() {
+    $("#image-Five").hover(function() {
+        $(this).find('#five').toggle(200);
+    
+        
+    });
+});
+
+$(function() {
+    $("#image-Six").hover(function() {
+        $(this).find('#final').toggle(200);
     
         
     });
